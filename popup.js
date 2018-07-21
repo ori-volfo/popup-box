7e33400272b6d6c9e3a98aaafea3748612685f98
ori-volfo <ori.volfo@gmail.com>
2018-07-21 14:26:38 +0300

    Project init

---
+++ popup.js
@@ -0,0 +1,43 @@
+var popUpBox = (function(msg){
+
+    var popUpBoxInit = function(){
+        var $popUp = $('#popup-box');
+        if($popUp.length){
+            return $popUp;
+        }else{ // single creation of HTML element
+            return $.get("popup-box/popup.php",function(html){
+                $(document.body).append(html);
+            });
+        }
+    };
+
+    var popUpBoxApply = function(){
+
+        var $popUp = $('#popup-box');
+        $popUp.show();
+        $popUp.find('.popup-header').html(msg.head);
+        if(msg.body){
+            $popUp.find('.popup-body').html(msg.body);
+        }
+        if(msg.btn){
+            $popUp.find('.btn').html(msg.btn);
+        }
+        if(msg.action){
+            $popUp.find('.btn').attr('onclick',msg.action);
+        }
+        if(msg.customCss){
+            $popUp.find('.popup-content').css(msg.customCss);
+        }
+        if(msg.bottom){
+            $popUp.find('.popup-bottom').html(msg.bottom);
+        }
+        $popUp.find('.close, .btn').click(function(){
+            $popUp.hide();
+        });
+    };
+
+    $.when( popUpBoxInit() ).then(function(){
+        popUpBoxApply();
+    });
+
+});
---
+++ popup.php
@@ -0,0 +1,14 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: *");
+?>
+<div id="popup-box">
+    <link rel="stylesheet" type="text/css" href="popup-box/popup.css">
+    <div class="popup-content">
+        <span class="close">&times;</span>
+        <h1 class="popup-header text-uppercase"></h1>
+        <div class="popup-body"></div>
+        <button type="button" class="btn text-uppercase account-bgcolor">OK</button>
+        <div class="popup-bottom"></div>
+    </div>
+</div>
